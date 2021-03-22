3f0cb522bbb4d1a7ddd5d1948447a9cb1c2a3ac8
didierrevelo <1829@holbertonschool.com>
2021-03-22 14:05:52 -0500

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 //Write a script that prints a message
-//depending of the number of arguments passed
 const arguments = process.argv.length;
 if (arguments === 2) {
     console.log('No argument');
