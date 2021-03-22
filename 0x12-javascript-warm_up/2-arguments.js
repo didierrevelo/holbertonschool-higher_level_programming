5985ebf00323fd7fc996c1432c7633a0f85494c0
didierrevelo <1829@holbertonschool.com>
2021-03-22 14:15:26 -0500

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 // Write a script that prints a message
 const arguments = process.argv.length;
 if (arguments === 2) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (arguments === 3) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
