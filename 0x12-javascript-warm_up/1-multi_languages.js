07944667a0c55b5bbfde7bdaddbc72d0f70236b2
didierrevelo <1829@holbertonschool.com>
2021-03-22 12:40:35 -0500

    corrections

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,6 +2,6 @@
 // tree lenguages    
 
 
-console.log("C is fun")
-console.log("Python is cool")
-console.log("JavaScript is amazing")
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
