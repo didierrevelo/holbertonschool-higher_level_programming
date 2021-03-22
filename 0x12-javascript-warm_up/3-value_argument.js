55fdbf88710e71e4965b1a55ed0c24b94011fd32
didierrevelo <1829@holbertonschool.com>
2021-03-22 14:57:19 -0500

    3-value_argument.js

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Write a script that prints the first argument
+const arguments = process.argv[2];
+if (arguments === undefined) {
+    console.log('No argument');
+}
+else {
+    console.log(arguments);    
+}
