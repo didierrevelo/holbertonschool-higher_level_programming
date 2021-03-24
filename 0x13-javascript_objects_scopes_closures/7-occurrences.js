39dccf8754c50a5b59208ac6a3dbf009d7879f92
didierrevelo <1829@holbertonschool.com>
2021-03-23 23:50:50 -0500

    7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Write a function that returns the number of occurrences in a list
-/* const countOccurrences = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);*/
+/* const countOccurrences = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0); */
 exports.nbOccurences = function (list, searchElement) {
   const count = list.reduce((a, v) => (v === searchElement ? a + 1 : a), 0);
   return count;
