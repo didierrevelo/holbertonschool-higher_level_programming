41511712531ba0cba1022debc31818b577402a6c
didierrevelo <1829@holbertonschool.com>
2021-04-09 16:42:06 -0500

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from urllib import request
+"""
+0-hbtn_status.py
+"""
+with request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+    print("Body respose:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    html2 = html.decode('utf-8')
+    print("\t- utf8 content: {}".format(html2))
