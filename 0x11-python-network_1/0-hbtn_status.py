bc863471aeee12e09f7218e99d32ffe5fc0e45dd
didierrevelo <1829@holbertonschool.com>
2021-04-09 17:13:26 -0500

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,8 +5,8 @@ import urllib.request
 """
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
-    print("Body respose:")
+    print("Body response:")
     print("\t- type: {}".format(type(html)))
     print("\t- content: {}".format(html))
-    html = html.decode('utf-8')
-    print("\t- utf8 content: {}".format(html))
+    html2 = html.decode('utf-8')
+    print("\t- utf8 content: {}".format(html2))
