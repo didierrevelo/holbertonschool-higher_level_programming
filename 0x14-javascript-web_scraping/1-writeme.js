3dd84ddcbc15afe9fd23a49de70e60ced0c56e2d
didierrevelo <1829@holbertonschool.com>
2021-05-04 23:05:49 -0500

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Write a script that writes a string to a file.
+const fs = require("fs");
+
+const data = process.argv;
+
+fs.writeFile(data[2], data[3], (err) => {
+  if (err) console.log(err);
+});
