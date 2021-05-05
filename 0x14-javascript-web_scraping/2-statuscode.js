bd05964b8a1c296acff1725a7f466d9ccb75bea7
didierrevelo <1829@holbertonschool.com>
2021-05-04 23:55:59 -0500

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 // Write a script that display the status code of a GET request.
-const request = (require('request'))
+const request = (require('request'));
 const data = process.argv;
 request(data[2], (err, res, body) => {
-    if (err) return console.log('Error: ', err);
-  
-    console.log('code: %d\n\n', res.statusCode);
-  });
+  if (err) return console.log('Error: ', err);
+  console.log('code: %d\n\n', res.statusCode);
+});
