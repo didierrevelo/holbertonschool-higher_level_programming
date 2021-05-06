c47ee8df8e31db7fede449b9f5b3021ae2e76453
didierrevelo <1829@holbertonschool.com>
2021-05-05 21:05:56 -0500

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Write a script that prints the number
+//of movies where the character “Wedge Antilles” is present.
+const request = require('request');
+const url = process.argv[2];
+let roller = 0
+request(url, function (error, response) {
+  if (error) { console.log(error); }
+  JSON.parse(response.body).results.forEach(function (result){
+    result.characters.forEach(id => id.includes('18') ? roller++ : roller)
+    return roller
+  });
+  console.log(roller)
+});
